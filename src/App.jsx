d822420a8edcdccdbe31cf2e92262a119daa7d6f
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2023-10-05 21:30:46 +0530

    Modifications on routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginComponent from "./components/login/login";
 import RegisterComponent from "./components/register/register";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
+  let url = window.location.href;
+  let status = false;
+
+  if (
+    url == "http://localhost:3000/auth/register" ||
+    url == "http://localhost:3000"
+  ) {
+    status = true;
+  }
   return (
     <Router>
+      {status ? <></> : <Navbar />}
       <Routes>
         <Route path="/" element={<LoginComponent />} exact />
         <Route path="/auth/register" element={<RegisterComponent />} exact />
